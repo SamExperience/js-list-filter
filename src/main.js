c14ad5b753a9b4b34d6b98de8f2e3d415931974d
Samuele Deriu <samuele.deriu.dev@gmail.com>
2026-09-21 19:54:31 +0200

    feat: render all users

--- src/main.js
+++ src/main.js
@@ -1,19 +1,28 @@
 import "./style.css";
+import { users } from "./data/data.js";
 
 document.querySelector("#app").innerHTML = `
-  <div class="container">
-    <h1>User List</h1>
+<div class="container">
+<h1>User List</h1>
 
-    <input type="text" id="searchInput" placeholder="Search users...">
+<input type="text" id="searchInput" placeholder="Search users...">
 
-    <section>
-      <h2>All Users</h2>
-      <div id="allUsers"></div>
-    </section>
+<section>
+<h2>All Users</h2>
+<div id="allUsers"></div>
+</section>
 
-    <section>
-      <h2>Admins</h2>
-      <div id="admins"></div>
-    </section>
-  </div>
+<section>
+<h2>Admins</h2>
+<div id="admins"></div>
+</section>
+</div>
 `;
+
+const contUsers = document.querySelector("#allUsers");
+
+users.forEach((user) => {
+  const li = document.createElement("li");
+  li.textContent = `${user.name} - isAdmin: ${user.isAdmin}`;
+  contUsers.appendChild(li);
+});
